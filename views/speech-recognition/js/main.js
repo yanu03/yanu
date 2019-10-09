5b8776d62c0f7354f6e838f6c0f2c7420a74cc3b
helperboys <bkhun1214@gmail.com>
2019-10-09 21:51:58 +0900

    helperboys

--- views/speech-recognition/js/main.js
+++ views/speech-recognition/js/main.js
@@ -64,15 +64,13 @@ $(function() {
     console.log('onresult', event);
 
     let interimTranscript = '';
-
-
-    for (let i = event.resultIndex; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) {
-        finalTranscript += event.results[i][0].transcript;
-      } else {
-        interimTranscript += event.results[i][0].transcript;
-      }
+    if (typeof event.results === 'undefined') {
+      recognition.onend = null;
+      recognition.stop();
+      return;
     }
+
+  
 
     finalTranscript = capitalize(finalTranscript);
     final_span.innerHTML = linebreak(finalTranscript);
